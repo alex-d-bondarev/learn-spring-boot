232b10c941624422c09f5beb7c38d80929a30e63
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2024-12-26 16:56:25 +0000

    Handle empty body exception

--- src/main/java/com/alex_d_bondarev/hello_spring/contacts/web/ApplicationExceptionHandler.java
+++ src/main/java/com/alex_d_bondarev/hello_spring/contacts/web/ApplicationExceptionHandler.java
@@ -2,8 +2,10 @@ package com.alex_d_bondarev.hello_spring.contacts.web;
 
 import com.alex_d_bondarev.hello_spring.contacts.exception.ContactNotFoundException;
 import com.alex_d_bondarev.hello_spring.contacts.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +15,13 @@ public class ApplicationExceptionHandler {
     @ExceptionHandler(ContactNotFoundException.class)
     public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleException(HttpMessageNotReadableException exception, HttpServletRequest request) {
+        String message = "Missing request body";
+        ErrorResponse errorResponse = new ErrorResponse(message);
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
