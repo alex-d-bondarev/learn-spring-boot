ca07d6d63d32c642b6086eca87824043a000c919
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2024-12-27 15:33:36 +0000

    Add gradesubmission v2

---
+++ src/main/java/com/alex_d_bondarev/hello_spring/gradesubmission/GradeSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.alex_d_bondarev.hello_spring.gradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+}
